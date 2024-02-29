2e71215bf308bef25108d2fda6fd831a9b91adf4
HarryPotter0107 <132452554+HarryPotter0107@users.noreply.github.com>
2024-02-29 19:34:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function stringChop(str, size) {
   // your code here
+	const result = [];
+
+	if(str==null || str.length==0) return result;
+	
+	size = size - 0;
+
+	let start = 0;
+
+	while(start < str.length){
+		console.log(start+" "+(start+size));
+		if(start+size < str.length){
+			result.push(str.slice(start,start+size));
+		}else{
+			result.push(str.slice(start, str.length));
+		}
+		start += (size);
+	}
+	return result;
+
 }
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
